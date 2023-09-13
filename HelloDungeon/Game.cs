9a19472210f9bcbc77b06553e41c2e256be61d60
TragerLeMaire <TraSL2004@Gmail.com>
2023-09-13 16:54:17 -0500

    Character selection and combat options added

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,80 +8,242 @@ namespace HelloDungeon
 {
     class Game
     {
-        void PrintStats(Monster monster)
+        //Weapon Struct
+        struct Weapon
         {
-            Console.WriteLine("Name: " + monster.Name);
-            Console.WriteLine("Health: " + monster.Health);
-            Console.WriteLine("Damage: " + monster.Damage);
-            Console.WriteLine("Defense: " + monster.Defense);
-            Console.WriteLine("Stamina: " + monster.Stamina);
+            public string Name;
+            public float Damage;
+            public float StaminaUsage;
         }
-        struct Monster
+        //Print player and opponet stats
+        void PrintStats(Character character)
         {
-
+            Console.WriteLine("Name: " + character.Name);
+            Console.WriteLine("Health: " + character.Health);
+            Console.WriteLine("Damage: " + character.Damage + "+" + character.CurrentWeapon.Damage);
+            Console.WriteLine("Defense: " + character.Defense);
+            Console.WriteLine("Stamina: " + character.Stamina);
+            Console.WriteLine("Weapon: " + character.CurrentWeapon.Name);
+        }
+        //struct for player and opponent
+        struct Character
+        {
             public string Name;
             public float Health;
             public float Damage;
             public float Defense;
             public float Stamina;
-
+            public Weapon CurrentWeapon;
         }
 
-        float Attack(ref Monster attacker, ref Monster defender)
+        bool gameOver = false;
+        int currentScene = 0;
+        string playerChoice = "";
+        
+        //Various characters
+        Character manWithLargeStick;
+        Character manWithAlligator;
+        Character gunDude;
+
+        Character player;
+
+        //Damage calculation function
+        float Attack(Character attacker,Character defender)
         {
-            float totalDamage = attacker.Damage - defender.Defense;
+            float totalDamage = attacker.Damage + attacker.CurrentWeapon.Damage - defender.Defense;
             return defender.Health - totalDamage;
         }
-        void Fight(Monster monster1, Monster monster2)
+
+        //Combat loop
+        void Fight(ref Character Character1, ref Character Character2)
         {
-
-            PrintStats(monster1);
-            PrintStats(monster2);
-
-            Console.WriteLine(monster1.Name + " fires on " + monster2.Name + "!!!");
-            monster2.Health = Attack(ref monster1,ref monster2);
+            Console.WriteLine(Character1.Name + " yeets their " +  player.CurrentWeapon.Name + " at " + Character2.Name + "!!!");
+            Character2.Health = Attack(Character1, Character2);
 
             Console.ReadKey(true);
-            PrintStats(monster1);
-            PrintStats(monster2);
+            PrintStats(Character1);
+            PrintStats(Character2);
 
-            Console.WriteLine(monster2.Name + " thwacks " + monster1.Name + "!!!");
-            monster1.Health = Attack(ref monster2,ref monster1);
+            Console.WriteLine(Character2.Name + " fires on " + Character1.Name + "!!!");
+            Character1.Health = Attack(Character2, Character1);
 
-            PrintStats(monster1);
-            PrintStats(monster2);
+            PrintStats(Character1);
+            PrintStats(Character2);
         }
-        float Healing(Monster healTarget, float healAmount)
+        
+        //Heal function
+        float Healing(Character healTarget, float healAmount)
         {
             return healTarget.Health + healAmount;
         }
+        
+        //Character selection menu
+        void CharacterSelection()
+        {
+            Console.WriteLine("Choose your character!");
+            Console.WriteLine("1.Florida Man with a large stick");
+            Console.WriteLine("2.Florida Man Ultimate");
+            Console.WriteLine("3.Dude with a gun");
+            Console.Write("> ");
+            playerChoice = Console.ReadLine();
+            if (playerChoice == "1")
+            {
+                player = manWithLargeStick;
+                currentScene = 1;
+
+            }
+            else if (playerChoice == "2")
+            {
+                player = manWithAlligator;
+                currentScene = 1;
+            }
+            else if (playerChoice == "3")
+            {
+                player = gunDude;
+                currentScene = 1;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+        }
+        
+        //Battle until a character dies
+        void BattleScene()
+        {
+            PrintStats(player);
+            playerChoice = "";
+            Console.WriteLine("Oh no! You are approached by a dude in a suit holding a gun! What will you do?");
+            Console.WriteLine("1.Fight him!");
+            Console.WriteLine("2.Defend yourself and pray!");
+            playerChoice = Console.ReadLine();
+
+            bool isDefending = false;
+            if (playerChoice == "1")
+            {
+                Fight(ref player, ref gunDude);
+
+                Console.Clear();
+            }
+            else if (playerChoice == "2")
+            {
+                Console.Clear();
+                isDefending = true;
+                player.Defense *= 4f;
+                Console.WriteLine("You put your weapon in front of you to guard against the attack!");
+                PrintStats(player);
+                PrintStats(gunDude);
+                Console.WriteLine(gunDude.Name + " won't let up on " + player.Name + "!!!");
+                player.Health = Attack(gunDude, player);
+                PrintStats(player);
+                PrintStats(gunDude);
+                Console.ReadKey();
+                Console.Clear();
+                if (isDefending == true)
+                {
+                    player.Defense /= 4f;
+                }
+
+            }
+            if (player.Health <= 0 || gunDude.Health <= 0)
+            {
+                currentScene = 2;
+            }
+
+            //Console.WriteLine("The Florida man chugs his Bud Lite!");
+            //Console.WriteLine(Healing(ManWithLargeStick, 35.7f));
+        }
+
+        //Winner display screen
+        void WinResultScene()
+        {
+            if (player.Health > 0f && gunDude.Health <= 0f)
+            {
+                Console.WriteLine("The winner is: " + player.Name);
+            }
+            else if (gunDude.Health > 0f && player.Health <= 0f)
+            {
+                Console.WriteLine("The winner is: " + gunDude.Name);
+            }
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+        
+        //Pre-game initilization 
+        void Start()
+        {
+            Weapon TruthStick;
+            TruthStick.Name = "Truth Hurts";
+            TruthStick.Damage = 50f;
+            TruthStick.StaminaUsage = 2f;
+
+            Weapon Alligator;
+            Alligator.Name = "Steve the Sentient Alligator Sword";
+            Alligator.Damage = 100f;
+            Alligator.StaminaUsage = 60f;
+
+            Weapon Gun;
+            Gun.Name = "Literally just a gun";
+            Gun.Damage = 100f;
+            Gun.StaminaUsage = 10f;
+            manWithLargeStick.Name = "Florida Man with a large stick";
+            manWithLargeStick.Health = 300f;
+            manWithLargeStick.Damage = 69.420f;
+            manWithLargeStick.Defense = 0.6f;
+            manWithLargeStick.Stamina = 10.0f;
+
+            manWithAlligator.Name = "Florida Man Ultimate";
+            manWithAlligator.Health = 500f;
+            manWithAlligator.Damage = 75f;
+            manWithAlligator.Defense = 50f;
+            manWithAlligator.Stamina = 60.0f;
+
+            gunDude.Name = "Dude with a gun";
+            gunDude.Health = 200f;
+            gunDude.Damage = 100f;
+            gunDude.Defense = 0f;
+            gunDude.Stamina = 100f;
+
+            manWithLargeStick.CurrentWeapon = TruthStick;
+            manWithAlligator.CurrentWeapon = Alligator;
+            gunDude.CurrentWeapon = Gun;
+        }
+
+        void Update()
+        {
+            if (currentScene == 0)
+            {
+                CharacterSelection();
+            }
+            else if (currentScene == 1)
+            {
+                BattleScene();
+            }
+            else if (currentScene == 2)
+            {
+                WinResultScene();
+            }
+        }
+
+        //Game ending message
+        void End()
+        {
+            Console.WriteLine("Thanks for playing!");
+        }
         public void Run()
         {
-            Monster ManWithLargeStick;
-            ManWithLargeStick.Name = "Man with a large stick";
-            ManWithLargeStick.Health = 300f;
-            ManWithLargeStick.Damage = 69.420f;
-            ManWithLargeStick.Defense = 0.6f;
-            ManWithLargeStick.Stamina = 10.0f;
+            Start();
 
-            Monster ManWithBiggerStick;
-            ManWithBiggerStick.Name = "Man with an EVEN BIGGER stick";
-            ManWithBiggerStick.Health = 400f;
-            ManWithBiggerStick.Damage = 75f;
-            ManWithBiggerStick.Defense = 2f;
-            ManWithBiggerStick.Stamina = 20.0f;
+            //Overall game loop
+            while (gameOver == false)
+            {
+                Update();
+            }
 
-            Monster GunDude;
-            GunDude.Name = "Dude with a gun";
-            GunDude.Health = 200f;
-            GunDude.Damage = 100f;
-            GunDude.Defense = 0f;
-            GunDude.Stamina = 100f;
-
-
-
-            Console.WriteLine("The stick man uses his soda!");
-            Console.WriteLine(Healing(ManWithLargeStick, 35.7f));
+            //Ending
+            End();
         }
     }
 }
